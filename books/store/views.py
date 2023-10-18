de151d8dd75c702feb895ec9d88c3e4fadb82cd7
Yeldana00 <keeseldana@gmail.com>
2023-10-18 23:12:07 +0600

    Initial commit

---
+++ books/store/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
