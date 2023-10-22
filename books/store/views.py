29d2fe57123e37e06b4aa5da1e6696554f4ee7f2
Yeldana00 <keeseldana@gmail.com>
2023-10-22 16:48:16 +0600

    3 lesson, filter, search, order

--- books/store/views.py
+++ books/store/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BooksSerializer
@@ -6,3 +8,10 @@ from .serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+    # second lesson filtering
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['price']
+    # add search
+    search_fields = ['name', 'author_name']
+    # add ordering
+    ordering_fields = ['price', 'autor_name']
